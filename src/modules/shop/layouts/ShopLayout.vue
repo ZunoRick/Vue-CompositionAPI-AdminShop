e98ba003b59966836c4baa312efa6977e9ad5a4a
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-05-03 22:58:50 -0600

    Agregando Layout para productos

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <TopMenu />
+
+  <RouterView />
+
+  <!-- Footer -->
+  <CustomFooter />
+</template>
+
+<script setup lang="ts">
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
