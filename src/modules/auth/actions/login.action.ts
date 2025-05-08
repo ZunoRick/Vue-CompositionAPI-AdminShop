58eb082029171c97f6325096ee1b1f194538e2e7
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-05-08 02:16:08 -0600

    Agregando primeras configuraciones de autenticación

---
+++ src/modules/auth/actions/login.action.ts
@@ -0,0 +1,42 @@
+import { isAxiosError } from 'axios';
+import { tesloApi } from '@/api/tesloApi';
+import type { AuthResponse, User } from '../interface';
+
+interface LoginError {
+  ok: false;
+  msg: string;
+}
+
+interface LoginSuccess {
+  ok: true;
+  user: User;
+  token: string;
+}
+
+export const loginAction = async (
+  email: string,
+  password: string,
+): Promise<LoginError | LoginSuccess> => {
+  try {
+    const { data } = await tesloApi.post<AuthResponse>('/auth/login', {
+      email,
+      password,
+    });
+    return {
+      ok: true,
+      user: data.user,
+      token: data.token,
+    };
+  } catch (error) {
+    // Error controlado
+    if (isAxiosError(error) && error.response?.status === 401) {
+      return {
+        ok: false,
+        msg: 'Usuario o contraseña incorrectos',
+      };
+    }
+
+    console.error(error);
+    throw new Error('No se pudo realizar la petición');
+  }
+};
